d754971f2eb4b14915200870ee8217f67f91c7b2
GodtoL <eliasvera793@gmail.com>
2025-01-02 18:49:42 -0300

    Se ajusta la recepcion de tags

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -4,17 +4,18 @@ const Book = require('../models/Book.js')
 
 const insertComment = async(req, res) => {
     try{
+        console.log("lo que llega" , req.body)
         const newComment = await Comment.create({
             content : req.body.content,
             votesCount : req.body.votesCount || 0,
             bookId: req.body.bookId,
-            userId: req.body.userId
+            userId: req.body.userId || 4
         })
         
-        res.status(201).json( { message : "Se creo correctaemente el comentario" } )
+        res.status(201).json( { message : "Se creo correctaemente el comentario"} )
     } catch (error) {
         res.status(500).json( { message : "No se pudo crear el comentario" } )
-        console.log("Erro al crear el comentario ", error)
+        console.log("Error al crear el comentario ", error)
     }
 }
 
