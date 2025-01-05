70857147df493d3f852a66f2470cf2b9eed90412
GodtoL <eliasvera793@gmail.com>
2025-01-04 23:12:12 -0300

    Correcion de nombres

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Configuración de la base de datos PostgreSQL
-const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
+const databaseUrl = process.env.DATABASE_URL;
+
+// Configuración de Sequelize para conectarse usando la URL
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: 'postgres', 
+  logging: false, 
 });
+
+module.exports = sequelize;
 
 // Conexión a la base de datos
 sequelize.authenticate()
